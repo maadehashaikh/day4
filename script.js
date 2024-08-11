a0039135f6c5dc3c11f0ef6112362c66c9ee6f68
Maadeha shaikh <maadeham@gmail.com>
2024-08-11 11:45:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const express = require('express')
+// is line say express ki sari chezain nikal kar aik express variable mai aa gai hain so that we can use it and by default express is a function  
+
+const app = express(); // app kion kay hm app create kar skty hain 
+
+// in 2 lines kay bad we can create routes (routes hain kia ? website kay name kay bad jo bh add ho are routes jaisy domain kay bad / mai .profile laga hua ho so that this is the route )
+
+app.get('/', function (req, res) {  //app say get and is mai we use sab say pehly route and then we write request handler => request handler middleware hota h and yeh hamesha aik function hota h so 
+  res.send('I am maadeha ')
+})
+
+app.get('/profile', function (req, res) { 
+  res.send('ap profile route hain and we use nodemon to make you automate');
+})
+
+app.listen(3000) // port kia hota h ? 
+
+
+// actually nodemon h kia bar bar hmara server off kar kay on karny p he changes reflect ho rhy thy ab nodemon ko install karny say hua yeh kay we have seen kay kaisy changes reflect karty he hmara server reload karny p changes dikh rhy hain ! we don't need to off and on the server again 
